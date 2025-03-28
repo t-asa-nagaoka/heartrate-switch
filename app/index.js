08528600f88d341eb12e2cb6cb1595d58cdcc6ba
t-asa-nagaoka <165289529+t-asa-nagaoka@users.noreply.github.com>
2025-03-28 12:47:06 +0900

    change app/index.js, companion/index.js, resources/index.view, settings/index.jsx

--- app/index.js
+++ app/index.js
@@ -9,15 +9,14 @@ import * as messaging from "messaging";
 appbit.appTimeoutEnabled = false;
 
 const SETTINGS_FILE = "settings.json";
-const LOOP_INTERVAL = 1000;
-const RESEND_INTERVAL = 5000;
+const RESEND_INTERVAL = 5;
 const ALLOW_MISSING_RATE = 0.05;
 const RELAX_HIGH = 3;
 const RELAX_NORMAL = 2;
 const RELAX_LOW = 1;
 const RELAX_NONE = 0;
 //const MIN_SAMPLES = 2;
-const VIBRATION_TIME = 1000;
+const VIBRATION_TIME_MS = 1000;
 
 //const imageRelaxMissingSamples = "blank.png";
 const imageRelaxHigh = "high_clear.png";
@@ -51,15 +50,25 @@ const state = {
   hrm: null,
   hrmTimestamp: null,
   relaxState: RELAX_NONE, // リラックス状態
-  timeline: [], // 時系列データ
+  distance: null, // LP 原点から重心までの長さ
+  area: null, // LP 楕円の面積
+  samples: [], // 心拍サンプル(欠損含む)
+  activeSamples: [], // 有効な心拍サンプル
+  activeSamplesCount: 0, // 有効サンプル数
+  retentionCount: 0, // 必要な有効サンプル保持数
   requests: [], // HTTPリクエスト(再送信キュー)
   preventDetection: false, // 検出抑制
-  samplesCount: 0, // 有効サンプル数
   detectionCount: 0, // 検出回数
   subjectiveCount: 0, // 主観スイッチ作動回数
   showImage: true, // アイコン表示
   allowSubjectiveSwitch: true, // 主観スイッチの作動を許可
 };
+
+const clock = {
+  measure: 0, // 心拍数計測
+  calculate: 0, // リラックス傾向の算出・更新
+  resend: 0, // HTTPリクエスト再送信
+}
 
 setup();
 
@@ -73,11 +82,10 @@ function registerHandlers() {
   if (HeartRateSensor) {
     state.hrm = new HeartRateSensor({ frequency: 1 });
     state.hrm.start();
-    setTimeout(onTimeoutLoop, LOOP_INTERVAL);
+    setTimeout(onTimeout, 1000);
   }
 
   messaging.peerSocket.addEventListener("message", onMessage);
-  setTimeout(onTimeoutResend, RESEND_INTERVAL);
 
   el.tileList.addEventListener("click", onClickTileList)
   el.showDetails.addEventListener("click", onClickShowDetails);
@@ -86,6 +94,8 @@ function registerHandlers() {
 
 function loadSettings() {
   const defaultSettings = {
+    measureInterval: 1,
+    calculateInterval: 1,
     retentionPeriod: 600,
     thresholdHigh: 1000,
     thresholdLow: 800,
@@ -116,58 +126,55 @@ function updateSettings(settings) {
   state.settings = settings;
 }
 
-function onTimeoutLoop() {
-  updateTimeline()
-  calculateSamplesCount()
-  
+function onTimeout() {
+  resendRequests()
+
+  updateSamples()
+
   if (activate()) {
     calculateRelax()
+    updateRelaxState()
+    detectLowRelax()
+    updatePreventDetection()
   }
 
-  updateRelaxState()
-  detectLowRelax()
-  updatePreventDetection()
   updateDisplay()
+  updateClock()
 
-  setTimeout(onTimeoutLoop, LOOP_INTERVAL);
+  setTimeout(onTimeout, 1000);
 }
 
-function updateTimeline() {
-  // 新しい時系列データを追加
-  const latest = initTimelineData()
+function updateSamples() {
+  // 新しい心拍サンプルを追加
   const {heartRate, timestamp} = state.hrm
+  const {retentionPeriod, measureInterval} = state.settings
 
-  if (heartRate && timestamp != state.hrmTimestamp) {
-    latest.duration = 60 * 1000 / heartRate
+  if (clock.measure == 0 && heartRate && timestamp != state.hrmTimestamp) {
+    const duration = 60 * 1000 / heartRate
+    state.samples.push(duration)
     state.hrmTimestamp = timestamp
+  } else {
+    state.samples.push(null)
   }
-  state.timeline.push(latest)
 
-  // 古い時系列データを削除
-  state.timeline = state.timeline.slice(-state.settings.retentionPeriod)
-}
+  // 古い心拍サンプルを削除
+  state.samples = state.samples.slice(-retentionPeriod)
 
-function initTimelineData() {
-  // 空の時系列データを返す
-  return {
-    duration: null,
-    distance: null,
-    area: null,
-  }
-}
+  // 必要な有効サンプル保持数を計算
+  state.retentionCount = Math.floor(retentionPeriod / measureInterval)
 
-function calculateSamplesCount() {
-  // 有効な心拍サンプル数を計算
-  state.samplesCount = state.timeline.filter(data => data.duration != null).length
+  // 有効なサンプルを抽出
+  state.activeSamples = state.samples.filter(sample => sample != null)
+  state.activeSamplesCount = state.activeSamples.length
 }
 
 function activate() {
   // リラックス傾向の計算が可能かどうか判断する
   // 起動直後からretentionPeriodと同じ秒数が経過するまでは計算不可
-  if (state.timeline.length == state.settings.retentionPeriod && state.samplesCount >= 2) {
+  if (clock.calculate == 0 && state.samples.length == state.settings.retentionPeriod && state.activeSamplesCount >= 2) {
     // 心拍サンプルの欠損率を算出する
     // サンプル数が2未満の場合はLP作図不可なため1とする
-    const missingRate = 1 - state.samplesCount / state.settings.retentionPeriod
+    const missingRate = 1 - state.activeSamplesCount / state.retentionCount
     return missingRate < ALLOW_MISSING_RATE
   }
 
@@ -176,8 +183,8 @@ function activate() {
 
 function calculateRelax() {
   // ローレンツプロットを生成
-  const samples = state.timeline.map(data => data.duration).filter(sample => sample != null)
-  const plots = samples.slice(1).map((current, i) => [samples[i], current])
+  const {activeSamples} = state
+  const plots = activeSamples.slice(1).map((current, i) => [activeSamples[i], current])
 
   // y=x軸 (x'軸), y=-x軸 (y'軸) への変換
   // 座標変換については LP_LOGIC_JAPANESE.pdf を参照
@@ -193,9 +200,8 @@ function calculateRelax() {
   const yDash = calculateStats(rotated.map(plot => plot[1]))
 
   // ローレンツプロットの原点から重心までの長さ・楕円の面積を算出
-  const latest = state.timeline[state.timeline.length - 1]
-  latest.distance = xDash.average
-  latest.area = Math.PI * xDash.stddev * yDash.stddev 
+  state.distance = xDash.average
+  state.area = Math.PI * xDash.stddev * yDash.stddev 
 }
 
 function calculateStats(values) {
@@ -210,18 +216,16 @@ function calculateStats(values) {
 
 function updateRelaxState() {
   // リラックス状態(低,通常,高)の更新
-  const latest = state.timeline[state.timeline.length - 1]
-  const distance = latest?.distance
 
-  //console.log(latest?.duration)
-  //console.log(distance)
-  //console.log(latest?.area)
+  //console.log(state.samples[state.samples.length - 1])
+  //console.log(state.distance)
+  //console.log(state.area)
 
-  if (distance == null) {
+  if (state.distance == null) {
     state.relaxState = RELAX_NONE
-  } else if (distance < state.settings.thresholdLow) {
+  } else if (state.distance < state.settings.thresholdLow) {
     state.relaxState = RELAX_LOW
-  } else if (distance > state.settings.thresholdHigh) {
+  } else if (state.distance > state.settings.thresholdHigh) {
     state.relaxState = RELAX_HIGH
   } else {
     state.relaxState = RELAX_NORMAL
@@ -259,25 +263,27 @@ function detectLowRelax() {
 
 function notify() {
   if (vibration.start("nudge")) {
-    setTimeout(() => vibration.stop(), VIBRATION_TIME);
+    setTimeout(() => vibration.stop(), VIBRATION_TIME_MS);
   }
 
   display.on = true;
 }
 
 function createRequest(subjective) {
-  const latest = state.timeline[state.timeline.length - 1]
-
   /** HTTPリクエストボディの生成 */
   return {
     /** 送信日時 */
     date: new Date().toISOString(),
     /** 現在のリラックス傾向(距離) ※旧仕様との互換性保持 */
-    relax: latest?.distance,
+    relax: state.distance ?? 0,
     /** 現在のリラックス傾向(距離) */
-    distance: latest?.distance,
+    distance: state.distance,
     /** 現在のリラックス傾向(面積) */
-    area: latest?.area,
+    area: state.area,
+    /** 心拍サンプルの計測間隔 */
+    measureInterval: state.settings.measureInterval,
+    /** リラックス傾向の計算・更新間隔 */
+    calculateInterval: state.settings.calculateInterval,
     /** 心拍サンプルの所持時間 */
     retentionPeriod: state.settings.retentionPeriod,
     /** 高リラックス状態のしきい値 */
@@ -346,19 +352,19 @@ function onMessage(event) {
   }
 }
 
-function onTimeoutResend() {
-  while (state.requests.length >= 1) {
-    const [request] = state.requests;
-    const sent = sendRequest(request);
-
-    if (!sent) {
-      return;
+function resendRequests() {
+  if (clock.resend == 0) {
+    while (state.requests.length >= 1) {
+      const [request] = state.requests;
+      const sent = sendRequest(request);
+  
+      if (!sent) {
+        return;
+      }
+  
+      state.requests.shift();
     }
-
-    state.requests.shift();
   }
-
-  setTimeout(onTimeoutResend, RESEND_INTERVAL)
 }
 
 function onClickTileList() {
@@ -400,7 +406,7 @@ function onClickSubjectiveSwitch() {
     setTimeout(() => {
       state.allowSubjectiveSwitch = true
       updateDisplay()
-    }, RESEND_INTERVAL);
+    }, RESEND_INTERVAL * 1000);
   }
 }
 
@@ -423,46 +429,52 @@ function updateDisplay() {
     el.showDetails.class = "text-button secondary application-fill hidden";
   }
 
-  const {samplesCount} = state
-  const {retentionPeriod} = state.settings
+  const {activeSamplesCount, retentionCount} = state
 
   switch (relaxState) {
     case RELAX_HIGH:
       el.image.href = imageRelaxHigh
-      el.label.text = `高 ${samplesCount} / ${retentionPeriod}`;
+      el.label.text = `高 ${activeSamplesCount} / ${retentionCount}`;
       break
     case RELAX_NORMAL:
       el.image.href = imageRelaxNormal
-      el.label.text = `通常 ${samplesCount} / ${retentionPeriod}`;
+      el.label.text = `通常 ${activeSamplesCount} / ${retentionCount}`;
       break
     case RELAX_LOW:
       el.image.href = imageRelaxLow
-      el.label.text = `低 ${samplesCount} / ${retentionPeriod}`;
+      el.label.text = `低 ${activeSamplesCount} / ${retentionCount}`;
       break
     case RELAX_NONE:
-      el.label.text = `蓄積中... ${samplesCount} / ${retentionPeriod}`;
+      el.label.text = `蓄積中... ${activeSamplesCount} / ${retentionCount}`;
       break
   }
 
-  const latest = state.timeline[state.timeline.length - 1]
-  const distance = latest?.distance
+  const {distance, area} = state
 
-  if (distance) {
-    const digits = distance < 1000 ? 1 : 0
-    el.currentRelax.text = `リラックス傾向:${distance.toFixed(digits)}`;
+  if (distance && area) {
+    const distanceDigits = distance < 1000 ? 1 : 0
+    const areaDigits = area < 1000 ? 1 : 0
+    el.currentRelax.text = `R:${distance.toFixed(distanceDigits)} (S:${area.toFixed(areaDigits)})`;
   } else {
-    el.currentRelax.text = `蓄積中... ${samplesCount} / ${retentionPeriod}`;
+    el.currentRelax.text = `蓄積中... ${activeSamplesCount} / ${retentionCount}`;
   }
   
   const {preventDetection, detectionCount, subjectiveCount} = state
-  const {thresholdHigh, thresholdLow, sendHttp} = state.settings
+  const {thresholdHigh, thresholdLow, measureInterval, calculateInterval, retentionPeriod, sendHttp} = state.settings
   const highDigits = thresholdHigh < 1000 ? 1 : 0;
   const lowDigits = thresholdLow < 1000 ? 1 : 0;
 
   el.thresholdHigh.text = `高しきい値:${thresholdHigh.toFixed(highDigits)}`;
   el.thresholdLow.text = `低しきい値:${thresholdLow.toFixed(lowDigits)}`;
-  el.retentionPeriod.text = `保持期間:${retentionPeriod}秒`
+  el.retentionPeriod.text = `計測:${measureInterval}s 算出:${calculateInterval}s 保持:${retentionPeriod}s`
   el.sendHttp.text = `HTTP送信:${sendHttp ? "ON" : "OFF"}`
   el.preventDetection.text = `検出抑制:${preventDetection ? "ON" : "OFF"}`
   el.count.text = `検出:${detectionCount}回 主観:${subjectiveCount}回`
+}
+
+function updateClock() {
+  const {measureInterval, calculateInterval} = state.settings
+  clock.measure = (clock.measure + 1) % measureInterval
+  clock.calculate = (clock.calculate + 1) % calculateInterval
+  clock.resend = (clock.resend + 1) % RESEND_INTERVAL
 }
--- settings/index.jsx
+++ settings/index.jsx
@@ -1,7 +1,9 @@
 function HelloWorld(props) {
   return (
     <Page>
-      <TextInput label="心拍数データの保持期間[秒]" placeholder="例：600" settingsKey="retentionPeriod" type="number"/>
+      <TextInput label="心拍数データの計測間隔[秒]" placeholder="例：1" settingsKey="measureInterval" type="number"/>
+      <TextInput label="リラックス傾向の算出・更新間隔[秒]" placeholder="例：1" settingsKey="calculateInterval" type="number"/>
+      <TextInput label="心拍数データの保持時間[秒]" placeholder="例：600" settingsKey="retentionPeriod" type="number"/>
       <TextInput label="高リラックス状態の閾値" placeholder="例：1000" settingsKey="thresholdHigh" type="number"/>
       <TextInput label="低リラックス状態の閾値" placeholder="例：800" settingsKey="thresholdLow" type="number"/>
       <Toggle settingsKey="sendHttp" label="低リラックス検出時のHTTPリクエスト送信"/>
